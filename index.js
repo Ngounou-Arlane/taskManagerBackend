cb776133c83d6b6c62b0607f3ec2719fa14f54bb
Ngounou Arlane Otantik Nuna <authenticnuna123@gmail.com>
2025-05-21 17:10:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-
+const MONGO_URI='mongodb+srv://otantik_nuna:demopass@cluster0.dkoyhul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const JWT_SECRET='MySuperSecretKey123'
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Connect MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
