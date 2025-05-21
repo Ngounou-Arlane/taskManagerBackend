e6605d79eb0e3b95d4e0c91e3fc75292f6592382
Ngounou Arlane Otantik Nuna <authenticnuna123@gmail.com>
2025-05-21 17:19:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-const express = require('express'); // Don't forget to require express
+const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const MONGO_URI = 'mongodb+srv://otantik_nuna:demopass@cluster0.dkoyhul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const JWT_SECRET = 'MySuperSecretKey123';
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-// Start the server
-const PORT = 10000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/tasks', require('./routes/tasks'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
